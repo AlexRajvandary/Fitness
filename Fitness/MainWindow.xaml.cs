fa9e1fefe4f6962781951ededa9f373beee2b33e
Alex Rajvandary <stark.11raj@gmail>
2022-06-02 02:50:02 +0300

    Добавлены реализация ICommand и основная вью-модель.

--- Fitness/MainWindow.xaml.cs
+++ Fitness/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Fitness.ViewModel;
 
 namespace Fitness
 {
@@ -22,6 +10,7 @@ namespace Fitness
     {
         public MainWindow()
         {
+            DataContext = new MainViewModel();
             InitializeComponent();
         }
     }
---
+++ Fitness/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using Fitness.Model;
+
+namespace Fitness.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            ExistingActivities = new ObservableCollection<Activity>();
+            ExistingFoods = new ObservableCollection<Food>();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Activity> ExistingActivities { get; set; }
+
+        public ObservableCollection<Food> ExistingFoods { get; set; }
+
+        private void OnPropertyChanged([CallerMemberName]string propName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
